6029ff39dc003ffddb800c20ef28d96e68ebb193
chloe0b0 <ChloeZeiter@gmail.com>
2022-07-25 14:04:22 -0700

    Type generic list added

---
+++ src/List.h
@@ -0,0 +1,49 @@
+#ifndef __LIST_H
+#define __LIST_H
+
+// Check for stdlib
+#ifndef EXIT_SUCCESS
+#include <stdlib.h>
+#endif
+
+// Growth factor of the array
+#define R 2
+
+// Type generic List structure implemented with a Dynamic Array Data Structure
+typedef struct List_t {
+    size_t cap, len;
+    void** data;
+} List_t;
+
+// Constructor
+List_t* Cons(size_t c0) {
+    List_t* xs = malloc(sizeof(List_t));
+
+    xs->data = calloc(c0, sizeof(void*));
+
+    xs->cap = c0;
+    xs->len = 0;
+
+    return xs;
+} 
+
+// Append to the end of the list
+void Append(List_t* xs, void* e) { 
+    // Resize is needed
+    if (xs->len == xs->cap) {
+        xs->data = realloc(xs->data, xs->cap * R);
+    }
+
+    xs->data[xs->len++] = e;
+}
+
+// Get len of list
+inline size_t len(List_t* xs) { return xs->len; }
+
+// Destructor
+void Destroy(List_t* xs) {
+    free(xs->data);
+    free(xs);
+}
+
+#endif
--- src/nerv.c
+++ src/nerv.c
@@ -1,1 +1,50 @@
-"" 
+#include <stdio.h>
+#include "List.h"
+
+typedef enum TOKEN_TYPE {
+    SUM,            // Add x to the current cell
+    SUB,            // Subtract x from the current cell
+    LOOP_START,     // Mark the begining of a loop
+    LOOP_END,       // Mark the end of a loop
+    SHR,            // Shift the memory ptr to the right by x
+    SHL             // Shift the memory ptr to the left by x
+} TOKEN_TYPE;
+
+typedef struct Token_t {
+    TOKEN_TYPE flag;
+    int n;          // number of times to apply the operation (computed by constant folding)
+    int jump;       // the position to jump if the current token is a loop
+} Token_t;
+
+// Convert Brainfuck Code to a set of Tokens
+// Use of constant folding to reduce redundancy and optimize the program
+// A basic example: 
+
+/*
+ *  +++++ would convert to a single Token of the following form
+ *
+ *  Token
+ *      {
+ *          flag = SUM,
+ *          lexeme = +++++,
+ *          n = 5
+ *      }
+ */
+// In this way, rather than applying SUM to the current cell N times we simply add N to the cell once 
+// The asymptotic time complexity of c + n, where c is the current cell, goes from linear to constant
+// The secondary optimization to start with will be storing loop jump locations in the token structures
+// This eliminates the need to repeatedley scan back and forth to find matching tokens, an expensive and unnecessary computation
+
+// Lexer
+List_t* Lexer(const char* p) {
+    List_t* Tokens = Cons(25);
+
+    return Tokens;
+}
+
+
+// Can interpret the tokens, or compile to C code to further optimize
+
+int main(void) {
+    printf("Hello Nerv!!");
+}
