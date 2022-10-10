78c59360ba7fdd0ec78e5a239d40efcc58f8c93b
chloe0x0 <ChloeZeiter@gmail.com>
2022-10-10 02:34:28 -0700

    Updated debug.c

--- src/debug.c
+++ src/debug.c
@@ -12,6 +12,10 @@ const char *tests[TESTS] = {"--++", "--+++", "++++++--[->+<]", "+++--", "+--", "
 void run(const char *p)
 {
     printf("Tokenizing: %s\n", p);
+    printf("-----------O0------------\n");
+    print_tokens(Lexer(p, O0), 0, 0);
+    printf("-----------O1------------\n");
+    print_tokens(Lexer(p, O1), 0, 0);
     printf("-----------O2------------\n");
     print_tokens(Lexer(p, O2), 0, 0);
 
