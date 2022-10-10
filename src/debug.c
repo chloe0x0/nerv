03efdf54d9b2a200adf0decc8cc90178ecfc15c0
chloe0x0 <ChloeZeiter@gmail.com>
2022-10-10 02:32:59 -0700

    Updated debug.c

--- src/debug.c
+++ src/debug.c
@@ -12,10 +12,6 @@ const char *tests[TESTS] = {"--++", "--+++", "++++++--[->+<]", "+++--", "+--", "
 void run(const char *p)
 {
     printf("Tokenizing: %s\n", p);
-    printf("-----------O0------------\n");
-    print_tokens(Lexer(p, O0), 0, 0);
-    printf("-----------O1------------\n");
-    print_tokens(Lexer(p, O1), 0, 0);
     printf("-----------O2------------\n");
     print_tokens(Lexer(p, O2), 0, 0);
 
@@ -26,14 +22,6 @@ void run(const char *p)
 
 int main(void)
 {   
-    //for (size_t i = 0; i < TESTS; ++i)
-    //    run(tests[i]);
-
-    char buffer[99999];
-    Read_BF("examples/Frac.bf", buffer, 99999);
-
-    clock_t t = clock();
-    nerv(buffer, O2);
-    t = clock() - t;
-    printf("\n%f\n", (double)t / CLOCKS_PER_SEC);
+    for (size_t i = 0; i < TESTS; ++i)
+        run(tests[i]);
 }
